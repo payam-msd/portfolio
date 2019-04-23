a7b59ff3d68fefe164cb54b5c68fe29ef4288ebb
payam-msd <payammgh17@gmail.com>
2019-04-23 12:56:58 +0430

    initialization

---
+++ Portfolio-v2/next.config.js
@@ -0,0 +1,5 @@
+const NextComposePlugins = require('next-compose-plugins');
+
+module.exports = NextComposePlugins([], {
+  /* nextConfig options here */
+});
---
+++ Portfolio-v2/pages/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Test = styled.p`
+  color: red;
+  font-size: 8rem;
+`;
+export default () => <Test>Hello World</Test>;
