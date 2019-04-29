124bd5114db895b1912bea5019f20819da62cbf4
payam-msd <payammgh17@gmail.com>
2019-04-29 16:26:07 +0430

    Add: contact & Resume pages

--- Portfolio-v2/pages/contact.js
+++ Portfolio-v2/pages/contact.js
@@ -3,9 +3,31 @@ import React, { Component } from 'react';
 export default class contact extends Component {
   render() {
     return (
-      <div>
-        <h1> THIS IS THE CONTACT PAGE</h1>
-      </div>
+      <>
+        <main>
+          <div className="split left__props">
+            <h1 className="textBox__name">
+              GET IN
+              <span className="textBox__family-sub">touch</span>
+            </h1>
+            <h2 className="textBox__mainText-2">Let's do something amazing</h2>
+            <div className="boxes">
+              <div className="boxes__context">
+                <span> payammgh17@gmail.com</span>
+              </div>
+              <div className="boxes__context">
+                <span>(+98) 903-190-1841</span>
+              </div>
+              <div className="boxes__context">
+                <span>Iran,Razavi-khorasan,mashhad</span>
+              </div>
+            </div>
+          </div>
+          <div className="split right__props">
+            <img className="right__img" src="/static/img/Img12.jpg" alt="img" />
+          </div>
+        </main>
+      </>
     );
   }
 }
--- Portfolio-v2/pages/resume.js
+++ Portfolio-v2/pages/resume.js
@@ -3,9 +3,45 @@ import React, { Component } from 'react';
 class Resume extends Component {
   render() {
     return (
-      <div>
-        <h1> THIS IS THE RESUME PAGE</h1>
-      </div>
+      <>
+        <main id="about">
+          <h1 className="textBox__title">
+            About
+            <span className="textBox__family-sub">Me</span>
+          </h1>
+
+          <div className="aboutMe">
+            <div className="aboutMe__bg-avatar">
+              <img
+                src="/static/img/portrait.jpg"
+                alt="bg-photo"
+                className="aboutMe__avatar"
+              />
+            </div>
+
+            <div lang="en" className="aboutMe__mainText">
+              <p>
+                I'm an Iranian Freelance Interactive Developer, Currently living
+                in Iran and I've been Working Independently. Autodidact
+                Developer. Interested in Graphic-Design. To This Day I Still
+                Design Personal Projects as well as Client work.
+              </p>
+              <p>
+                This atypical Background Allows me To be Very Sensitive to
+                visual details, animation or Typography. Lorem ipsum dolor sit
+                amet consectetur adipisicing elit. Adipisci, facilis. Recusandae
+                amet est quos officia qui illo, ab ad esse iure numquam voluptas
+                laborum. Unde natus voluptas ipsum obcaecati architecto.
+              </p>
+              <p>
+                when i'm not working i love to try new gastronomic restaurants
+                or cafe with friends, hitchhiking cities, holding english
+                events, foremost, lose my self in the internet
+              </p>
+            </div>
+          </div>
+        </main>
+      </>
     );
   }
 }
